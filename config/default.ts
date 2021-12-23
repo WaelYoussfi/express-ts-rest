a37edda2df61eea83874ba50f2a955be038a415f
Wael <youssfi.wael1997@gmail.com>
2021-12-23 23:27:33 +0100

    changing database name and extra configs

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,4 @@
 export default {
     port: 3000, //you can change the port number
-    dbUri: "mongodb://localhost:27017/name_of_database", //add the uri to the database you want to work with
+    dbUri: "mongodb://localhost:27017/user_products", //add the uri to the database you want to work with
 };
