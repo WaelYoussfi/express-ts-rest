c46f3f5048a83970f7f3f3ca22d69d25ba8c7ac2
Wael <youssfi.wael1997@gmail.com>
2021-12-23 23:56:39 +0100

    adding user model, user schema and user service

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
-    port: 3000, //you can change the port number
-    dbUri: "mongodb://localhost:27017/user_products", //add the uri to the database you want to work with
+    port: 3000,
+    dbUri: "mongodb://localhost:27017/user_products",
+    saltWorkFactor: 10,
 };
